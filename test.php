8e573c2752acb7977b40c64768c5df9c0743abfc
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-28 15:12:14 -0300

    Update test.php

--- test.php
+++ test.php
@@ -6,8 +6,18 @@
     $resultado = json_decode(curl_exec($ch));
     //var_dump ($resultado);
     foreach($resultado->data as $agente){
-        echo "Nome: " . $agente->displayName . "<br>";
-        echo "Descrição: " . $agente->description . "<br>";
-        echo "Nome de desenvolvimento: " . $agente->developerName . "<br>";
+        echo "Nome: " . $agente->abilities[0]->displayName . "<br>";
+        echo "Descrição: " . $agente->abilities[0]->description . "<br>";
+        echo "<tr></tr>" . "<br>";
+        /*echo "Nome: " . $agente->abilities[1]->displayName . "<br>";
+        echo "Descrição: " . $agente->abilities[1]->description . "<br>";
+        echo "<tr></tr>" . "<br>";
+        echo "Nome: " . $agente->abilities[2]->displayName . "<br>";
+        echo "Descrição: " . $agente->abilities[2]->description . "<br>";
+        echo "<tr></tr>" . "<br>";
+        echo "Nome: " . $agente->abilities[3]->displayName . "<br>";
+        echo "Descrição: " . $agente->abilities[3]->description . "<br>";
+        echo "<tr></tr>" . "<br>";*/
+        //echo "Nome de desenvolvimento: " . $agente->developerName . "<br>";
     }
 ?>
