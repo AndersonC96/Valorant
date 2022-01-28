457202ed706e7224ee113d7f57bdd63b04c04f52
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-28 16:38:03 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,16 @@
 <?php
-    $url = 'https://valorant-api.com/v1/competitivetiers?language=pt-BR';
+    error_reporting(E_ERROR | E_PARSE);
+    $url = 'https://valorant-api.com/v1/gamemodes?language=pt-BR';
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $resultado = json_decode(curl_exec($ch));
     //var_dump ($resultado);
     foreach($resultado->data as $agente){
-        echo "Nome: " . implode(',',$agente->tiers[0]->tierName) . "<br>";
-        echo "Descrição: " . $agente->extraDescription . "<br>";
-        echo "Descrição 2: " . $agente->promoDescription . "<br>";
-        echo "Descrição 3: " . $agente->useAdditionalContext . "<br>";
+        echo "Descrição: " . $agente->teamRoles[0] . "<br>";
+        echo "Descrição 2: " . $agente->gameFeatureOverrides[0]->featureName . "<br>";
+        echo "Descrição 3: " . $agente->gameRuleBoolOverrides[0]->featureName . "<br>";
+        echo "Descrição 4: " . $agente->useAdditionalContext . "<br>";
         echo "<tr></tr>" . "<br>";
         /*echo "Nome: " . $agente->abilities[1]->displayName . "<br>";
         echo "Descrição: " . $agente->abilities[1]->description . "<br>";
