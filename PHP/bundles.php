49e5de7ab1038e4612fb9a5d2656680cdfbadb26
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-28 15:49:56 -0300

    Update bundles.php

--- PHP/bundles.php
+++ PHP/bundles.php
@@ -3,8 +3,8 @@
 ?>
 <html>
     <head>
-        <title>Valorant | Agentes</title>
-        <link rel="shortcut icon" href="./favicon.png"/>
+        <title>Valorant | Bundles</title>
+        <link rel="shortcut icon" href="../images/favicon.png"/>
         <embed name="myMusic" loop="true" hidden="true" src="./music.mp3">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -12,7 +12,6 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="../css/table.css" />
-        <link rel="shortcut icon" href="../images/favicon.png"/>
     </head>
     <body>
         <?php
