9596f0963570bb03df2d1e6482478158bf55bb3c
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-27 23:41:57 -0300

    Create specs.php

---
+++ specs.php
@@ -0,0 +1,86 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Valorant | Specs</title>
+        <link rel="stylesheet" href="css/normalize.css" />
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                    <a href="agents.php">Agentes</a>
+                    <a href="buddies.php">Companheiros</a>
+                    <a href="bundles.php">Bundles</a>
+                    <a href="ceremonies.php">Cerimônias</a>
+                    <a href="competitive_tiers.php">Níveis competitivos</a>
+                    <a href="content_tiers.php">Níveis de conteúdo</a>
+                    <a href="contracts.php">Contratos</a>
+                    <a href="currencies.php">Moedas</a>
+                    <a href="events.php">Eventos</a>
+                    <a href="gamemodes.php">Modos de jogo</a>
+                    <a href="gear.php">Equipamento</a>
+                    <a href="maps.php">Mapas</a>
+                    <a href="player_cards.php">Cartas de jogador</a>
+                    <a href="player_titles.php">Títulos dos Jogadores</a>
+                    <a href="seasons.php">Temporadas</a>
+                    <a href="sprays.php">Sprays</a>
+                    <a href="themes.php">Temas</a>
+                    <a href="weapons.php">Armas</a>
+                    <a href="version.php">Versão</a>
+                    <a href="specs.php">Specs</a>
+                </li>
+            </ul>
+        </nav>
+        <header class="specsIntro">
+            <h1>Specs</h1>
+        </header>
+        <main class="hardwareIntro">
+            <div class="hardwareBlock">
+                <h2>Requisitos para PC</h2>
+                <div class="hardwareBlock2">
+                    <div>
+                        <img src="images/computer.png" alt="Image of Computer"/>
+                    </div>
+                    <p>Windows // 7/8/10/11 64-bit</p>
+                    <p>RAM // 4GB</p>
+                    <p>VRAM // 1GB</p>
+                </div>
+                <div class="hardwareBlock3">
+                    <div class="hardwareSub1">
+                        <div class="miniBlock1">
+                            <img src="images/dash.png" alt="Image of Dash"/>
+                        </div>
+                        <p>Especificações mínimas / 30 FPS</p>
+                        <p>Especificações recomendadas / 60 FPS</p>
+                        <p>High-end specs / 144+ FPS</p>
+                    </div>
+                    <div class="hardwareSub2">
+                        <div class="miniBlock2">
+                            <img src="images/cpu.png" alt="Image of Dash"/>
+                        </div>
+                        <p>Intel Core 2 Duo E8400 (Intel), Athlon 200GE (AMD)</p>
+                        <p>Intel i3-4150 (Intel), Ryzen 3 1200 (AMD)</p>
+                        <p>Intel i5-9400F 2.90GHz (Intel), Ryzen 5 2600X (AMD)</p>
+                    </div>
+                    <div class="hardwareSub3">
+                        <div class="miniBlock3">
+                            <img src="images/gpu.png" alt="Image of Dash"/>
+                        </div>
+                        <p>Intel HD 4000, Radeon R5 200</p>
+                        <p>Geforce GT 730, Radeon R7 240</p>
+                        <p>GTX 1050 Ti, Radeon R7 370</p>
+                    </div>
+                </div>
+            </div>
+            No Windows 11, também exigimos "TPM 2.0 e UEFI Secure Boot."
+        </main>
+        <footer>
+            <p>
+                &copy; 2020 Riot Games, inc, Riot Games, <span>Valorant</span>, e quaisquer logotipos associados são marcas comerciais, marcas de serviço e/ou marcas registradas da Riot Games, inc. Todas as outras marcas comerciais são de propriedade de seus respectivos proprietários.
+            </p>
+        </footer>
+    </body>
+</html>
