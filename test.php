a2f71bc6a5b757fdbb549214b2b5e604a7d2669f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-28 16:13:02 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
-    $url = 'https://valorant-api.com/v1/ceremonies?language=pt-BR';
+    $url = 'https://valorant-api.com/v1/competitivetiers?language=pt-BR';
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $resultado = json_decode(curl_exec($ch));
     //var_dump ($resultado);
     foreach($resultado->data as $agente){
-        echo "Nome: " . $agente->displayNameSubText . "<br>";
+        echo "Nome: " . implode(',',$agente->tiers[0]->tierName) . "<br>";
         echo "Descrição: " . $agente->extraDescription . "<br>";
         echo "Descrição 2: " . $agente->promoDescription . "<br>";
         echo "Descrição 3: " . $agente->useAdditionalContext . "<br>";
