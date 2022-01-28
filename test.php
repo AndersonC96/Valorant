0b306b26f42224c4329a1f0875046841304106a6
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-28 17:18:04 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,14 @@
 <?php
     error_reporting(E_ERROR | E_PARSE);
-    $url = 'https://valorant-api.com/v1/gamemodes?language=pt-BR';
+    $url = 'https://valorant-api.com/v1/sprays?language=pt-BR';
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $resultado = json_decode(curl_exec($ch));
     //var_dump ($resultado);
     foreach($resultado->data as $agente){
-        echo "Descrição: " . $agente->teamRoles[0] . "<br>";
-        echo "Descrição 2: " . $agente->gameFeatureOverrides[0]->featureName . "<br>";
+        echo "Descrição: " . $agente->category . "<br>";
+        echo "Descrição 2: " . $agente->themeUuid . "<br>";
         echo "Descrição 3: " . $agente->gameRuleBoolOverrides[0]->featureName . "<br>";
         echo "Descrição 4: " . $agente->useAdditionalContext . "<br>";
         echo "<tr></tr>" . "<br>";
