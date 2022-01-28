3dc531f849ecb9f1b88b1d79a469ff76ba4e71e3
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-27 23:04:49 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,97 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Valorant</title>
+        <link rel="stylesheet" href="css/normalize.css" />
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                    <a href="agents.php">Agentes</a>
+                    <a href="buddies.php">Companheiros</a>
+                    <a href="bundles.php">Bundles</a>
+                    <a href="ceremonies.php">Cerimônias</a>
+                    <a href="competitive_tiers.php">Níveis competitivos</a>
+                    <a href="content_tiers.php">Níveis de conteúdo</a>
+                    <a href="contracts.php">Contratos</a>
+                    <a href="currencies.php">Moedas</a>
+                    <a href="events.php">Eventos</a>
+                    <a href="gamemodes.php">Modos de jogo</a>
+                    <a href="gear.php">Equipamento</a>
+                    <a href="maps.php">Mapas</a>
+                    <a href="player_cards.php">Cartas de jogador</a>
+                    <a href="player_titles.php">Títulos dos Jogadores</a>
+                    <a href="seasons.php">Temporadas</a>
+                    <a href="sprays.php">Sprays</a>
+                    <a href="themes.php">Temas</a>
+                    <a href="weapons.php">Armas</a>
+                    <a href="version.php">Versão</a>
+                </li>
+            </ul>
+        </nav>
+        <header class="indexHeader">
+            <p>Um shooter tático baseado em personagens 5v5</p>
+            <h1>Valorant</h1>
+            <a href="https://playvalorant.com/pt-br/">jogar agora</a>
+        </header>
+        <div class="valorantIntro">
+            <h1 id="intro">Nós somos Valorant</h1>
+            <div class="introColumn">
+                <div class="introLeft">
+                    <h2>Desafiar os limites</h2>
+                    <p>
+                        <span>Valorant</span> é o seu estágio competitivo global. É um jogo de tiro tático 5v5
+                        para plantar ou desarmar o Spike em uma vida por rodada, primeira a 13 série. Mais do que armas e balas, Mais do que armas e balas, você vai escolher um agente armado com habilidades adaptativas, rápidas e letais que criam oportunidades para deixar seu tiroteio brilhar.
+                        <br />
+                        <br />
+                        A criatividade é a sua maior arma.
+                    </p>
+                </div>
+                <div class="introRight">
+                    <div>
+                        <img src="images/introImage.jpg" alt="Intro Image"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="agentsIntro">
+            <div class="agentsColumn">
+                <div class="agentsRight">
+                    <h1>Os agentes</h1>
+                    <div class="agentsSub">
+                        <h2>Plantar a estaca</h2>
+                        <p>
+                            Os agentes e suas habilidades de assinatura são como você ganha com habilidade e estilo. Adaptar e superar no papel de um Duelista, Iniciador, Controlador ou Sentinela.
+                        </p>
+                    </div>
+                    <a href="agents.php">Ver todos os agentes</a>
+                </div>
+                <div class="agentsLeft">
+                    <div>
+                        <img src="images/reyna.png" alt="Agent Image"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="mapsIntro">
+            <div class="mapsColumn">
+                <div class="mapsLeft">
+                    <h1>Os mapas</h1>
+                    <h2>Detonar ou desativar</h2>
+                    <p>
+                        Haven. Bind. Split. Ascent. Cada etapa para uma infinidade de jogadas experientes, garras de agente e estratégias de equipe.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <p>
+                &copy; 2020 Riot Games, inc, Riot Games, <span>Valorant</span>, e quaisquer logotipos associados são marcas comerciais, marcas de serviço e/ou marcas registradas da Riot Games, inc. Todas as outras marcas comerciais são de propriedade de seus respectivos proprietários.
+            </p>
+        </footer>
+    </body>
+</html>
