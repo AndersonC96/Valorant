8b469ab71d393c5b637c50027491629ef89bff4d
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-07-29 18:10:15 -0300

    Update agents.php

--- PHP/agents.php
+++ PHP/agents.php
@@ -1,14 +1,42 @@
 <?php
     error_reporting(E_ERROR | E_PARSE);
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="pt-BR">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Valorant | Agentes</title>
         <link rel="shortcut icon" href="../images/favicon.png"/>
         <embed name="myMusic" loop="true" hidden="true" src="./music.mp3">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="../css/table.css" />
+        <style>
+            .navbar-brand {
+                font-weight: bold;
+                color: #ff4655 !important;
+            }
+            .navbar-nav .nav-link {
+                color: #ffffff !important;
+                margin-right: 10px;
+            }
+            .navbar-nav .nav-link.active {
+                color: #ff4655 !important;
+                font-weight: bold;
+            }
+            .dropdown-menu {
+                background-color: #1c1c1c;
+                border: none;
+            }
+            .dropdown-menu .dropdown-item {
+                color: #ffffff;
+            }
+            .dropdown-menu .dropdown-item:hover {
+                background-color: #ff4655;
+                color: #ffffff;
+            }
+        </style>
     </head>
     <body>
         <?php
@@ -17,16 +45,11 @@
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             $resultado = json_decode(curl_exec($ch));
-            //var_dump ($resultado);
-            /*foreach($resultado->data as $agente){
-                echo "Nome: " . $agente->displayName . "<br>";
-                echo "Descrição: " . $agente->description . "<br>";
-                echo "Nome de desenvolvimento: " . $agente->developerName . "<br>";
-            }*/
         ?>
         <div class="container">
-            <nav class="navbar navbar-expand-lg bg-danger">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Valorant API</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -60,7 +83,7 @@
                                 <a class="nav-link" href="gamemodes.php">Modos de jogo</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="../specs.php">Specs</a>
+                                <a class="nav-link" href="../specs.php">Specs</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Mais</a>
@@ -81,13 +104,13 @@
                     </div>
                 </div>
             </nav>
-            <h2>Agentes</h2>
+            <h2 class="mt-4">Agentes</h2>
             <div class="row">
                 <?php
                     foreach($resultado->data as $agente){
                         $id = str_replace("https://valorant-api.com/v1/agents/", "", $agente->uuid);
                 ?>
-                <div class="col-3">
+                <div class="col-md-3 mb-4">
                     <div class="card" style="width: 18rem;">
                         <img class="card-img-top" src="<?php echo $agente->displayIcon; ?>" alt="Imagem do agente <?php echo $agente->displayName; ?>">
                         <div class="card-body">
@@ -104,7 +127,6 @@
                                         }
                                     ?>
                                 </ul>
-                                <br>
                             </p>
                         </div>
                     </div>
