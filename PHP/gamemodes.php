e9c21e059d5a13426a6a66e6f39e3c2fbf2167cd
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-07-30 10:52:52 -0300

    Update gamemodes.php

--- PHP/gamemodes.php
+++ PHP/gamemodes.php
@@ -1,14 +1,42 @@
 <?php
     error_reporting(E_ERROR | E_PARSE);
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="pt-BR">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Valorant | Modos de jogo</title>
         <link rel="shortcut icon" href="../images/favicon.png"/>
         <embed name="myMusic" loop="true" hidden="true" src="./music.mp3">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="../css/table.css" />
+        <style>
+            .navbar-brand{
+                font-weight: bold;
+                color: #ff4655 !important;
+            }
+            .navbar-nav .nav-link{
+                color: #ffffff !important;
+                margin-right: 10px;
+            }
+            .navbar-nav .nav-link.active{
+                color: #ff4655 !important;
+                font-weight: bold;
+            }
+            .dropdown-menu{
+                background-color: #1c1c1c;
+                border: none;
+            }
+            .dropdown-menu .dropdown-item{
+                color: #ffffff;
+            }
+            .dropdown-menu .dropdown-item:hover{
+                background-color: #ff4655;
+                color: #ffffff;
+            }
+        </style>
     </head>
     <body>
         <?php
@@ -19,8 +47,9 @@
             $resultado = json_decode(curl_exec($ch));
         ?>
         <div class="container">
-            <nav class="navbar navbar-expand-lg bg-danger">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Valorant API</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -75,19 +104,19 @@
                     </div>
                 </div>
             </nav>
-            <h2>Modos de jogo</h2>
+            <h2 class="mt-4">Modos de jogo</h2>
             <div class="row">
                 <?php
-                    foreach($resultado->data as $agente){
-                        $id = str_replace("https://valorant-api.com/v1/gamemodes/", "", $agente->uuid);
+                    foreach($resultado->data as $modo){
+                        $id = str_replace("https://valorant-api.com/v1/gamemodes/", "", $modo->uuid);
                 ?>
-                <div class="col-3">
+                <div class="col-md-3 mb-4">
                     <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="<?php echo $agente->displayIcon; ?>" alt="Imagem do agente <?php echo $agente->displayName; ?>">
+                        <img class="card-img-top" src="<?php echo $modo->displayIcon; ?>" alt="Imagem do modo <?php echo $modo->displayName; ?>">
                         <div class="card-body">
-                            <h5 class="card-title" style="color: #ff4655"><?php echo $agente->displayName; ?></h5>
+                            <h5 class="card-title" style="color: #ff4655"><?php echo $modo->displayName; ?></h5>
                             <p class="card-text">
-                                <b style="color: #ff4655">Duração:</b> <?php echo $agente->duration; ?><br>
+                                <b style="color: #ff4655">Duração:</b> <?php echo $modo->duration; ?><br>
                             </p>
                         </div>
                     </div>
